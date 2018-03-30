ca56773a5d9d3b213d43e858a87b85f398afb760
jongyook2 <emrrn92@naver.com>
2018-03-30 12:43:57 +0900

    update new src, javadocs, and single line comments

--- java/edu/handong/csee/java/lab06/Buginfestation.java
+++ java/edu/handong/csee/java/lab06/Buginfestation.java
@@ -35,7 +35,9 @@ import java.util.Scanner; //import standard input from keyboard
 	 */
 	public void computeNumOfBugsToUseKiller() { // public void omputeNumOfBugsToUseKiller method declaration
 		Scanner keepWork =new Scanner(System.in); // instantiate Class Scanner to keyboard(input)
+		System.out.print("Enter the total volume of your house in cubic feet: "); //output(Enter the total volume of your house in cubic feet:) is shown in your monitor
 		double houseVolume = keepWork.nextDouble(); //bouseVolume is input(double type) from keyboard
+		System.out.print("Enter the estimated number of raches in your house: "); //output(Enter the estimated number of raches in your house:) is shown in your monitor
 		double startPopulation = keepWork.nextDouble(); //startPopulation is input(double type) from keyboard
 		
 		double population = startPopulation; //declare local variable population and initialization to startPopulation
@@ -47,15 +49,16 @@ import java.util.Scanner; //import standard input from keyboard
 		while(totalBugVolume < houseVolume) { //loop by the time totalBugVolume is larger than houseVolume
 			double newBugs = population * GROWTH_RATE; //initiate local varible newBugs and initialization to population * GROWTH_RATE
 			double newBugVolume = newBugs * ONE_BUG_VOLUME; //initiate local varible newBugsVolume and initialization to newBugs * ONE_BUG_VOLUME
-			population = population + newBugs; //add newBugs to existing population
-			totalBugVolume = population * ONE_BUG_VOLUME; //new population * ONE_BUG_VOLUME is saved into totalBugVolume
+			population = population + newBugs; //add newBugs to current population
+			totalBugVolume = totalBugVolume + newBugVolume; //add newBugVolume to current totalBugVolume
 			countWeeks = countWeeks + 1; //countWeeks increases by 1
 		}
 		
-		System.out.println("startPop=" + startPopulation); //output(startPop= startPopulation(value)) is shown in your monitor
-		System.out.println("houseVol=" + houseVolume); //output(houseVol= houseVolume(value)) is shown in your monitor
-		System.out.println("countWeek=" +countWeeks); //output(countWeek= countWeeks(value)) is shown in your monitor
-		System.out.println("pop=" + (int)population); //output(pop= population(value)) is shown in your monitor
-		System.out.println("total bug vol=" + (int)totalBugVolume); //output(total bug vol= totalBugVolume(value)) is shown in your monitor
+		System.out.println("Strating with a roach population of " + (int)startPopulation); //output(Strating with a roach population of startPopulation(value)) is shown in your monitor
+		System.out.println("and a house with a Volume of " + houseVolume+" cubic feet."); //output(and a house with a Volume of houseVolume(value)) is shown in your monitor
+		System.out.println("after " +countWeeks+" weeks"); //output(after countWeeks(value) weeks) is shown in your monitor
+		System.out.println("the house will be filed with " + (int)population+ " roaches"); //output(the house will be filed with population(value)) is shown in your monitor
+		System.out.println("They will fill a volume of " + (int)totalBugVolume+ " cubic feet."); //output(They will fill a volume of totalBugVolume(value)) is shown in your monitor
+		System.out.println("Better call Debugging Experts Inc."); //output(Better call Debugging Experts Inc.) is shown in your monitor
 		}
 }
